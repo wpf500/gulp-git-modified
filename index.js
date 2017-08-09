ee0cd614b39169f18d733dc32359e15adef014a2
Will Franklin <will@willpf.co.uk>
2017-08-09 17:30:21 +0100

    Update

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
 var exec = require('child_process').exec;
+var path = require('path');
 var through = require('through2');
 
-function modified() {
+var cache = {};
+
+function modified(basePath) {
+    basePath = basePath || '.';
+
     return through.obj(function (file, enc, callback) {
-        if (!file.isDirectory()) {
+        if (file.isDirectory()) {
+            callback(null, file);
+        } else {
             exec('git log -1 --pretty=format:%at "' + file.path + '"', {}, function (e, out) {
-                file.modified = out.trim();
+                var filePath = './' + path.join(basePath, file.relative);
+                cache[filePath] = filePath + '?v=' + out.trim();
                 callback(null, file);
             });
         }
-
-        callback();
     });
 }
 
 module.exports = modified;
+module.exports.cache = cache;
